b66b57e119a210d08321dc06f73580c39c63e0b9
xiaoyusu <1225074067@qq.com>
2026-09-02 21:30:00 +0800

    发送短信验证码

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -36,8 +36,9 @@ public class UserController {
      */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-        // TODO 发送短信验证码并保存验证码
-        return Result.fail("功能未完成");
+        //  发送短信验证码并保存验证码
+
+        return userService.sendCode(phone,session);
     }
 
     /**
@@ -46,7 +47,7 @@ public class UserController {
      */
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
-        // TODO 实现登录功能
+        //  TODO 实现登录功能
         return Result.fail("功能未完成");
     }
 
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.Hutool;
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -14,7 +21,30 @@ import org.springframework.stereotype.Service;
  * @author 虎哥
  * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /**
+     * 发送验证吗
+     * @param phone
+     * @param session
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //1.判断手机号是否正确
+        if(RegexUtils.isPhoneInvalid(phone)){
+            //2.不符合，返回错误信息
+            return Result.fail("手机格式错误");
+        }
+
+        //3.符合，生成验证吗
+        String code = RandomUtil.randomNumbers(6);
+        //4.将验证码存入session
+        session.setAttribute("code",code);
+        //5.发送验证码
+        log.debug("发送短信验证码成功：{}",code);
+        //返回ok
+        return Result.ok();
+    }
 }
